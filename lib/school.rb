213c1f7d9e1b41e29d62fa818276e1295b088764
Nataliya R <n.rudenko@gmail.com>
2019-03-03 18:22:32 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -21,7 +21,8 @@ hash = {}
 hash["new_key"] << "new_value_for_value_array"
 hash["new_key"] = []
 hash["new_key"] << "new_value_for_value_array"
-
+hash
+ => {"new_key"=>["new_value_for_value_array"]}
 
  end 
 
