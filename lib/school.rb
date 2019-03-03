bc4b68763b924b980c018751dc0eb771c79a4d41
Nataliya R <n.rudenko@gmail.com>
2019-03-03 18:20:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -16,12 +16,6 @@ class School
  def add_student(student, grade)
    @student
    @grade
-    hash["new_key"] << "new_value_for_value_array"
- hash["new_key"] = []
-hash["new_key"] << "new_value_for_value_array"
- 
-hash
- => {"new_key"=>["new_value_for_value_array"]} 
  end 
 
 
