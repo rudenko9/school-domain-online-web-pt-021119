6db60ab664662c4a40ddf49ba634afdd0f9a9df0
Nataliya R <n.rudenko@gmail.com>
2019-03-03 17:10:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,20 +1,7 @@
 class School
   def initialize(roster)
-    @roster = roster
-  end 
-  def roster=(roster)
-    @roster = roster
-  end 
-  
-  def roster
-    @roster
-  end 
-  def school=(school)
-    @school = school
-  end
-  def school
-    @school
-  end 
+ end 
+
 end 
 school = School.new("Bayside High School")
 school.roster
