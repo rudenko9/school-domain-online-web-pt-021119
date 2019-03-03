2ed4cbd1cacef74f863ff161cafe901d5b5a51af
Nataliya R <n.rudenko@gmail.com>
2019-03-03 16:39:33 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,12 @@
 class School
+  attr_accessor :roster
   attr_reader :school
   
   def initialize(school)
     @school = school 
   end 
+
+  
 end 
   
 school = School.new("Bayside High School")
